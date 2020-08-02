61fd1cf5bd4c46cc94af418bcc874c0d0f7d8a79
EumYoungHyun <opwer032@naver.com>
2020-08-02 10:32:44 +0900

    [200802 음영현] 연반추 학습법

--- study/NodeJS/chapter1_just_typing/main.js
+++ study/NodeJS/chapter1_just_typing/main.js
@@ -12,9 +12,6 @@ var connectHandler = function connected() {
     eventEmitter.emit("data_received");
 };
 
-// connection이벤트와 connectHandler 이벤트 핸들러를 연동
-eventEmitter.on("connection", connectHandler);
-
 // data_received 이벤트와 익명 함수와 연동
 // 함수를 변수안에 담는 대신에, .on() 메소드의 인자로 직접 함수를 전달
 eventEmitter.on("data_received", function () {
@@ -23,5 +20,8 @@ eventEmitter.on("data_received", function () {
 
 // connection 이벤트 발생시키기
 eventEmitter.emit("connection");
+
+// connection이벤트와 connectHandler 이벤트 핸들러를 연동
+eventEmitter.on("connection", connectHandler);
 
 console.log("Program has ended");
