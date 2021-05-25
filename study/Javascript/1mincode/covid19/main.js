6f4b7a8386ea0bc8e8eebfc8cb958413853cc015
Eum <opwer032@naver.com>
2021-05-25 10:48:18 +0900

    feat: Add basic logic

---
+++ study/Javascript/1mincode/covid19/main.js
@@ -0,0 +1,48 @@
+(() => {
+    const imageElements = document.querySelectorAll(".graphic-item");
+    const stepElements = document.querySelectorAll(".step");
+
+    let currentItem = imageElements[0];
+    let ioIndex;
+
+    const io = new IntersectionObserver((entries, observer) => {
+        ioIndex = entries[0].target.dataset.index * 1;
+    });
+
+    for (let i = 0; i < stepElements.length; i++) {
+        io.observe(stepElements[i]);
+        stepElements[i].dataset.index = i;
+        imageElements[i].dataset.index = i;
+    }
+
+    function activate() {
+        currentItem.classList.add("visible");
+    }
+
+    function inactivate() {
+        currentItem.classList.remove("visible");
+    }
+
+    window.addEventListener("scroll", () => {
+        let step;
+        let boundingRect;
+
+        for (let i = ioIndex - 1; i < ioIndex + 2; i++) {
+            step = stepElements[i];
+            if (!step) continue;
+
+            boundingRect = step.getBoundingClientRect();
+
+            if (
+                boundingRect.top > window.innerHeight * 0.1 &&
+                boundingRect.top < window.innerHeight * 0.8
+            ) {
+                inactivate();
+                currentItem = imageElements[step.dataset.index];
+                activate();
+            }
+        }
+    });
+
+    activate();
+})();
